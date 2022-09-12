4ab18a293e06ea261e926193a6a000e35c53b791
KukJinKim0211 <rnrwls0211@naver.com>
2022-09-13 00:08:24 +0900

    vanilla에서 redux 쓰기

---
+++ vanilla-redux/src/index.js
@@ -0,0 +1,30 @@
+import { createStore } from "redux";
+
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
+const ADD = "ADD";
+const MINUS = "MINUS";
+
+const countModifier = (state = 0, action) => {
+  switch (action.type) {
+    case ADD:
+      return state + 1;
+    case MINUS:
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+const countStore = createStore(countModifier);
+
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+
+countStore.subscribe(onChange);
+
+add.addEventListener("click", () => countStore.dispatch({ type: ADD }));
+minus.addEventListener("click", () => countStore.dispatch({ type: MINUS }));
